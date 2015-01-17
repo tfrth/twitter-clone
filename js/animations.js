3ab49a2aefe2cc8ffe9d725cf9b520127efe6e66
Ty Frith <tyfrith@gmail.com>
2015-01-16 17:00:00 -0700

    updated

--- js/animations.js
+++ js/animations.js
@@ -3,26 +3,33 @@ $( document ).ready(function() {
 
 $('.tweet-compose').on('click', function() {
 	$('#tweet-controls').css( 'visibility', 'visible'); //set tweet controls to be visible when clicked (changed css)
-	$(this).css('height', '5em'); //increased height of tweet text box
-	
-
-    })
-
-$('.tweet-compose').on('keypress', function() {
-	var tweetLength = $(this).val().length + 1;
-	$('#char-count')
-	
-
-})	
-
-
-
-
-$('#tweetContent').on('click', function() {
-	var tweet = $('.tweet-compose').val();
-	$('#stream').prepend(tweet);
-	$('#stream').prepend(markup);
+	$('.tweet-compose').css('height', '5em'); //increased height of tweet text box
 })
+
+$('.tweet-compose').on('keyup', function() {
+	$('#char-count').html(140);
+	var tweetLength = $(this).val().length + 1;
+	$('#char-count').html(140 - tweetLength);
+
+
+
+
+
+
+
+});
+
+
+	
+
+
+
+
+// $('#tweetContent').on('click', function() {
+// 	var tweet = $('.tweet-compose').val();
+// 	$('#stream').prepend(tweet);
+// 	$('#stream').prepend(markup);
+// })
     	
 
   
